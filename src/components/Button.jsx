78717c4025d9f2759bcffee3705cccd6b3ed170a
Luis Gonzalez <lgonzalez23@verizon.net>
2024-03-01 15:19:53 -0800

    Button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -11,14 +11,24 @@ const Button = ({
   const classes = `button relative inline-flex items-center justify-center h-11 transition-colors hover:text-color-1 ${px} ${
     white ? "text-n-8" : "text-n-1"
   } ${className}`;
+  const spanClasses = "relative z-10";
   const renderButton = () => (
     <button className={classes}>
-      <span>{children}</span>
+      <span className={spanClasses}>{children}</span>
       {ButtonSvg(white)}
     </button>
   );
 
-  return renderButton();
+  const renderLink = () => (
+    <a className={classes} href={href}>
+      <span className={spanClasses}>{children}</span>
+      {ButtonSvg(white)}
+    </a>
+  );
+
+  if (!href) return renderButton();
+
+  return renderLink();
 };
 
 export default Button;
