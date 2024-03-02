db45aed56c420a40e2989079f5b4b04600972504
Luis Gonzalez <lgonzalez23@verizon.net>
2024-03-02 15:06:42 -0800

    hamburger menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,7 +12,13 @@ const Header = () => {
     useState(false);
 
   return (
-    <div className="bg-n-8/90 fixed left-0 top-0 z-50 backdrop-blur-sm border-b border-n-6 lg:bg-n-8/90 lg:backdropbackdrop-blur-sm w-full">
+    <div
+      className={`bg-n-8/90 fixed left-0 top-0 z-50 backdrop-blur-sm border-b border-n-6 lg:bg-n-8/90 lg:backdropbackdrop-blur-sm w-full ${
+        openNavigation
+          ? "bg-n-8"
+          : "bg-n-8/90 backdrop-blur-sm"
+      }`}
+    >
       <div className="flex justify-between items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4 py-2">
         <a href="#hero" className="block w-[12rem] xl:mr-8">
           <img
