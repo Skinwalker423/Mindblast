80dd6216935e668cf791ee6a2adb32f14bdacd80
Luis Gonzalez <lgonzalez23@verizon.net>
2024-03-02 17:58:26 -0800

    lock scroll

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,42 @@
+import {
+  disablePageScroll,
+  enablePageScroll,
+} from "scroll-lock";
 import { useLocation } from "react-router-dom";
+
+import { useState } from "react";
+
 import { brainwave } from "../assets";
-import { navigation } from "../constants";
 import Button from "./Button";
 import MenuSvg from "../assets/svg/MenuSvg";
 import { HambugerMenu } from "./design/Header";
-import { useState } from "react";
+
+import { navigation } from "../constants";
 
 const Header = () => {
   const location = useLocation();
+
   const [openNavigation, setOpenNavigation] =
     useState(false);
 
   const handleToggleNav = () => {
+    if (openNavigation) {
+      enablePageScroll();
+    } else {
+      disablePageScroll();
+    }
     setOpenNavigation((prev) => !prev);
   };
 
   const handleClick = () => {
+    if (!openNavigation) return;
+    enablePageScroll();
     setOpenNavigation(false);
   };
 
   return (
     <div
-      className={`bg-n-8/90 fixed left-0 top-0 z-50 backdrop-blur-sm border-b border-n-6 lg:bg-n-8/90 lg:backdropbackdrop-blur-sm w-full ${
+      className={`fixed left-0 top-0 z-50 border-b border-n-6 lg:bg-n-8/90 lg:backdropbackdrop-blur-sm w-full ${
         openNavigation
           ? "bg-n-8"
           : "bg-n-8/90 backdrop-blur-sm"
@@ -42,23 +57,32 @@ const Header = () => {
           } fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent`}
         >
           <div className="relative flex flex-col justify-center items-center z-2 m-auto lg:flex-row">
-            {navigation.map((item) => (
-              <a
-                className={`block relative font-code text-2xl uppercase text-n-1 transition-colors hover:text-color-1 ${
-                  item.onlyMobile ? "lg:hidden" : ""
-                } px-6 py-6 md:py-8 lg:mr-0.25 lg:text-xs lg:font-semibold ${
-                  location.hash === item.url
-                    ? "z-2 lg:text-n-1"
-                    : "text-n-1/50"
-                } leading-5 lg:hover:text-n-1 xl:px-12`}
-                key={item.id}
-                href={item.url}
-              >
-                {item.title}
-              </a>
-            ))}
-            <HambugerMenu />
+            {navigation.map((item) => {
+              console.log(
+                "paths",
+                item.url,
+                "location",
+                location.hash
+              );
+              return (
+                <a
+                  onClick={handleClick}
+                  className={`block relative font-code text-2xl uppercase text-n-1 transition-colors hover:text-color-1 ${
+                    item.onlyMobile ? "lg:hidden" : ""
+                  } px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs lg:font-semibold ${
+                    location.hash === item.url
+                      ? "z-2 lg:text-n-1"
+                      : "text-n-1/50"
+                  } leading-5 lg:hover:text-n-1 xl:px-12`}
+                  key={item.id}
+                  href={item.url}
+                >
+                  {item.title}
+                </a>
+              );
+            })}
           </div>
+          <HambugerMenu />
         </nav>
         <a
           className="button hidden mr-8 text-n-1/50 transition-colors hover:text-n-1 lg:block"
