b5e41c6021a3a556d61e8de036194586a59cc426
Luis Gonzalez <lgonzalez23@verizon.net>
2024-03-02 17:14:47 -0800

    handle click

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,6 +11,14 @@ const Header = () => {
   const [openNavigation, setOpenNavigation] =
     useState(false);
 
+  const handleToggleNav = () => {
+    setOpenNavigation((prev) => !prev);
+  };
+
+  const handleClick = () => {
+    setOpenNavigation(false);
+  };
+
   return (
     <div
       className={`bg-n-8/90 fixed left-0 top-0 z-50 backdrop-blur-sm border-b border-n-6 lg:bg-n-8/90 lg:backdropbackdrop-blur-sm w-full ${
@@ -28,7 +36,11 @@ const Header = () => {
             height={40}
           />
         </a>
-        <nav className="hidden fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent">
+        <nav
+          className={`${
+            openNavigation ? "flex" : "hidden"
+          } fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent`}
+        >
           <div className="relative flex flex-col justify-center items-center z-2 m-auto lg:flex-row">
             {navigation.map((item) => (
               <a
@@ -45,6 +57,7 @@ const Header = () => {
                 {item.title}
               </a>
             ))}
+            <HambugerMenu />
           </div>
         </nav>
         <a
@@ -55,36 +68,13 @@ const Header = () => {
         </a>
         <Button className="hidden lg:flex">Sign in</Button>
         <Button
-          className="block lg:hidden"
-          onClick={() => {
-            console.log("clicked");
-            setOpenNavigation((prev) => !prev);
-          }}
+          className="ml-auto lg:hidden"
+          onClick={handleToggleNav}
+          px="px-3"
         >
-          {MenuSvg({ openNavigation })}
+          <MenuSvg openNavigation={openNavigation} />
         </Button>
       </div>
-      {openNavigation && (
-        <div className="h-screen flex flex-col justify-center items-center gap-2">
-          {navigation.map((item) => (
-            <a
-              className={`block relative font-code text-2xl uppercase text-n-1 transition-colors hover:text-color-1 ${
-                item.onlyMobile ? "lg:hidden" : ""
-              } px-6 py-6 md:py-8 lg:mr-0.25 lg:text-xs lg:font-semibold ${
-                location.hash === item.url
-                  ? "z-2 lg:text-n-1"
-                  : "text-n-1/50"
-              } leading-5 lg:hover:text-n-1 xl:px-12`}
-              key={item.id}
-              href={item.url}
-            >
-              {item.title}
-            </a>
-          ))}
-
-          <HambugerMenu />
-        </div>
-      )}
     </div>
   );
 };
