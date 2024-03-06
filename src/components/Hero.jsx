d758e3c7d448bef97e3d678c479e170b5951a8c6
Luis Gonzalez <lgonzalez23@verizon.net>
2024-03-06 15:55:03 -0800

    hero image styling

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Section from "./Section";
-import { curve } from "../assets";
+import { curve, robot } from "../assets";
+import Button from "./Button";
 
 const Hero = () => {
   return (
     <Section
-      className={"pt-[12rem] -mt-[5.25rem]"}
+      className={"pt-32 sm:pt-[12rem] -mt-[5.25rem]"}
       crosses
       crossesOffset={"lg:translate-y-[5.25rem]"}
       customPaddings
@@ -15,9 +16,9 @@ const Hero = () => {
         <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[4rem] md:mb-20 lg:mb-[6rem]">
           <h1 className="h1 mb-6">
             Explore the Possibilities of AI Chatting with
-            BrainwaveCurve
+            MinblastCurve
             <span className="inline-block relative">
-              Brainwave{" "}
+              Mindblast{" "}
               <img
                 src={curve}
                 alt="curve"
@@ -27,6 +28,29 @@ const Hero = () => {
               />{" "}
             </span>
           </h1>
+          <p className="body-1 max-w-3xl mx-auto mb-6 text-n-2 lg:mb-8">
+            Unlease the power of AI within Mindblast.
+            Upgrade your productivity with Mindblast, the
+            open AI chat app.
+          </p>
+          <Button white href={"/pricing"}>
+            Get Started
+          </Button>
+        </div>
+        <div className="relative max-w-[23rem] mx-auto md:max-w-5xl xl:mb-24">
+          <div className="relative z-1 p-0.5 rounded-2xl bg-conic-gradient">
+            <div className="relative bg-n-8 rounded-[1rem]">
+              <div className="h-[1.4rem] bg-n-10 rounded-t-[0.9rem]" />
+              <div className="aspect-[33/40] rounded-b-[0.9] overflow-hidden md:aspect-[688/490] lg:aspect-[1024/490]">
+                <img
+                  src={robot}
+                  alt="hero"
+                  width={1440}
+                  height={1800}
+                />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </Section>
