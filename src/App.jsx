4320055bbea40141b8ee8cd993424d28a0c3fcee
Luis Gonzalez <lgonzalez23@verizon.net>
2024-03-06 14:12:53 -0800

    Section

--- src/App.jsx
+++ src/App.jsx
@@ -7,60 +7,7 @@ const App = () => {
       <div className="pt-16 lg:pt-20 overflow-hidden">
         <Header />
       </div>
-      <p className="mt-20">
-        Lorem ipsum dolor sit, amet consectetur adipisicing
-        elit. Magnam vel culpa minus debitis eligendi beatae
-        qui consequatur sint accusantium similique ad dolor
-        libero, vitae fuga porro aliquam at? Fugiat, quas.
-      </p>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing
-        elit. Magnam vel culpa minus debitis eligendi beatae
-        qui consequatur sint accusantium similique ad dolor
-        libero, vitae fuga porro aliquam at? Fugiat, quas.
-      </p>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing
-        elit. Magnam vel culpa minus debitis eligendi beatae
-        qui consequatur sint accusantium similique ad dolor
-        libero, vitae fuga porro aliquam at? Fugiat, quas.
-      </p>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing
-        elit. Magnam vel culpa minus debitis eligendi beatae
-        qui consequatur sint accusantium similique ad dolor
-        libero, vitae fuga porro aliquam at? Fugiat, quas.
-      </p>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing
-        elit. Magnam vel culpa minus debitis eligendi beatae
-        qui consequatur sint accusantium similique ad dolor
-        libero, vitae fuga porro aliquam at? Fugiat, quas.
-      </p>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing
-        elit. Magnam vel culpa minus debitis eligendi beatae
-        qui consequatur sint accusantium similique ad dolor
-        libero, vitae fuga porro aliquam at? Fugiat, quas.
-      </p>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing
-        elit. Magnam vel culpa minus debitis eligendi beatae
-        qui consequatur sint accusantium similique ad dolor
-        libero, vitae fuga porro aliquam at? Fugiat, quas.
-      </p>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing
-        elit. Magnam vel culpa minus debitis eligendi beatae
-        qui consequatur sint accusantium similique ad dolor
-        libero, vitae fuga porro aliquam at? Fugiat, quas.
-      </p>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing
-        elit. Magnam vel culpa minus debitis eligendi beatae
-        qui consequatur sint accusantium similique ad dolor
-        libero, vitae fuga porro aliquam at? Fugiat, quas.
-      </p>
+
       <ButtonGradient />
     </>
   );
