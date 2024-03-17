cb98f1bf19793f42fc654a08d36d1197c1668334
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2024-03-16 19:11:14 -0700

    bottom line

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -26,7 +26,8 @@ const Hero = () => {
       >
         <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[4rem] md:mb-20 lg:mb-[6rem]">
           <h1 className="h1 mb-8">
-            Explore the Possibilities of AI Chatting with{" "}
+            Explore the Possibilities
+            of&nbsp;AI&nbsp;Chatting with{" "}
             <span className="inline-block relative">
               Mindblast{" "}
               <img
@@ -91,6 +92,7 @@ const Hero = () => {
           <BackgroundCircles parallaxRef={parallaxRef} />
         </div>
       </div>
+      <BottomLine />
     </Section>
   );
 };
