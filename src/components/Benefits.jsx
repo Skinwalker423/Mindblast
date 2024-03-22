78d06d816528af6ab9c90b93bd44c469a13dcc3a
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2024-03-22 16:21:02 -0700

    benefit card

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -1,18 +1,34 @@
 import React from "react";
 import Section from "./Section";
 import Heading from "./Heading";
+import { benefits } from "../constants";
 
 const Benefits = () => {
   return (
     <Section id={"features"}>
       <div className="container relative z-2">
         <Heading
-          className=""
+          className="md:max-w-md lg:max-w-2xl"
           title={"Chat Smarter, Not Harder with Mindblast"}
-          text={"testing 123"}
         />
+        <div className="flex flex-wrap gap-10 mb-10 z-10">
+          {benefits.map((benefit) => {
+            return (
+              <div
+                className="block z-10 relative p-0.5 bg-no-repeat bg-[length:100%_100%] md:max-w-[24rem]"
+                key={benefit.id}
+                style={{
+                  backgroundImage: `url("${benefit.backgroundUrl}")`,
+                }}
+              >
+                <div>
+                  <h5>{benefit.title}</h5>
+                </div>
+              </div>
+            );
+          })}
+        </div>
       </div>
-      ;
     </Section>
   );
 };
