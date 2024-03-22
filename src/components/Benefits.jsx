9081aa8aa42f9e45016415fcb42f442cd5c8d2db
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2024-03-21 19:43:33 -0700

    heading

---
+++ src/components/Benefits.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Section from "./Section";
+import Heading from "./Heading";
+
+const Benefits = () => {
+  return (
+    <Section id={"features"}>
+      <div className="container relative z-2">
+        <Heading
+          className=""
+          title={"Chat Smarter, Not Harder with Mindblast"}
+          text={"testing 123"}
+        />
+      </div>
+      ;
+    </Section>
+  );
+};
+
+export default Benefits;
